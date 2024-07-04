75ab0c154a78237e89f3b2cbbc3788bc6052a284
Nikon-33 <89101717800@mail.ru>
2024-07-04 13:40:18 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -28,3 +28,22 @@ for x in range(1, 20):
             pass_.append(y)
             continue
 print(pass_) # Не смог придумать как исключить повторяющиеся варианты, без потери данных (1 или нескольких пар чисел)
+
+# Следгка доработанный вариант
+import random
+
+num = random.randint(3, 20)
+print("Подберем пароль для значения - ", num)
+pass_ = ""
+
+for x in range(1, 20):
+    for y in range(1, 20):
+        sum = x + y
+        if num % int(sum) == 0:
+            pass_ = pass_ + str(x)
+            pass_ = pass_ + str(y)
+            continue
+if num % 2 == 0:
+    print(pass_)
+else:
+    print(pass_[:int(len(pass_)) // 2])
