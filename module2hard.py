b0427a7b92b6182c0ee45e1e50f42bdf109001c0
Nikon-33 <89101717800@mail.ru>
2024-07-05 12:08:28 +0300

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -2,48 +2,16 @@ import random
 
 num = random.randint(3, 20)
 print("Подберем пароль для значения - ", num)
-pass_ = {}
-
-for x in range(1, 20):
-    for y in range(1, 20):
-        sum = x + y
-        if num % int(sum) == 0:
-            pass_.update({x: y})
-            continue
-
-print(pass_)    # минус данного способа в том, что ключи с одним названием перезаписываются
-                # и такие варианты как 1+1 и 1+2 не сохраняются месте
-
-import random
-
-num = random.randint(3, 20)
-print("Подберем пароль для значения - ", num)
-pass_ = []
-
-for x in range(1, 20):
-    for y in range(1, 20):
-        sum = x + y
-        if num % int(sum) == 0:
-            pass_.append(x)
-            pass_.append(y)
-            continue
-print(pass_) # Не смог придумать как исключить повторяющиеся варианты, без потери данных (1 или нескольких пар чисел)
-
-# Следгка доработанный вариант
-import random
-
-num = random.randint(3, 20)
-print("Подберем пароль для значения - ", num)
 pass_ = ""
 
-for x in range(1, 20):
-    for y in range(1, 20):
+for x in range(1, 9):
+    for y in range(2, 20):
         sum = x + y
-        if num % int(sum) == 0:
+        if x == y or x > y:
+            continue
+        elif num % int(sum) == 0:
             pass_ = pass_ + str(x)
             pass_ = pass_ + str(y)
-            continue
-if num % 2 == 0:
-    print(pass_)
-else:
-    print(pass_[:int(len(pass_)) // 2])
+
+
+print(pass_)
